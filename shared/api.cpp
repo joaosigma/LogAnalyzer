1a38a059af9018642b0f81bb0141ab057c998916
João Ramos <joao.sigma@gmail.com>
2020-06-14 17:31:47 +0100

    Finish C api wrapper

--- shared/api.cpp
+++ shared/api.cpp
@@ -2,45 +2,235 @@
 
 #include <lines_repo.hpp>
 
-const char** la_list_files(laFlavorType flavor, const char* folderPath, int* numFiles)
+namespace
 {
+	laStrUTF8 convertStr(std::string_view str)
+	{
+		if (str.empty())
+			return la_str_init();
+
+		auto newStr = la_str_init_length(static_cast<int>(str.size()));
+		std::memcpy(newStr.data, str.data(), str.size());
+
+		return newStr;
+	}
+
+	la::LinesRepo::FindContext::FindOptions convertSearchOptions(const laSearchOptions options)
+	{
+		switch (options)
+		{
+		case LA_SEARCH_OPTION_CASE_SENSITIVE:
+			return la::LinesRepo::FindContext::FindOptions::CaseSensitive;
+		case LA_SEARCH_OPTION_NONE:
+		default:
+			return la::LinesRepo::FindContext::FindOptions::None;
+		}
+	}
+
+	la::LinesRepo::ExportOptions convertExportOptions(const laExportOptions* options)
+	{
+		if (!options)
+			return {};
+
+		la::LinesRepo::ExportOptions nOptions;
+		nOptions.filePath = { options->filePath.data, static_cast<size_t>(options->filePath.size) };
+		nOptions.appendToFile = (options->appendToFile != 0);
+
+		switch (options->translationType)
+		{
+		case LA_TRANSLATOR_TYPE_RAW_JSON:
+			nOptions.translationType = la::TranslatorsRepo::Type::RawJSON;
+			break;
+		case LA_TRANSLATOR_TYPE_TRANSLATED:
+			nOptions.translationType = la::TranslatorsRepo::Type::Translated;
+			break;
+		case LA_TRANSLATOR_TYPE_TRANSLATED_JSON:
+			nOptions.translationType = la::TranslatorsRepo::Type::TranslatedJSON;
+			break;
+		case LA_TRANSLATOR_TYPE_RAW:
+		default:
+			nOptions.translationType = la::TranslatorsRepo::Type::Raw;
+			break;
+		}
+
+		return nOptions;
+	}
+}
+
+laStrUTF8 la_str_init()
+{
+	laStrUTF8 str;
+	str.data = nullptr;
+	str.size = 0;
+
+	return str;
+}
+
+laStrUTF8 la_str_init_copy(const char* cstr)
+{
+	if (!cstr || (*cstr == '\0'))
+		return la_str_init();
+
+	auto str = la_str_init_length(static_cast<int>(std::strlen(cstr)));
+	std::memcpy(str.data, cstr, str.size);
+
+	return str;
+}
+
+laStrUTF8 la_str_init_length(int size)
+{
+	if (size <= 0)
+		return la_str_init();
+
+	laStrUTF8 str;
+	str.size = size;
+	str.data = reinterpret_cast<char*>(std::malloc(str.size));
+
+	return (str.data ? str : la_str_init());
+}
+
+void la_str_destroy(laStrUTF8* str)
+{
+	if (!str)
+		return;
+
+	if (str->data)
+		std::free(str->data);
+
+	str->data = nullptr;
+	str->size = 0;
+}
+
+laStrFixedUTF8 la_str_fixed_init()
+{
+	laStrFixedUTF8 str;
+	str.data = nullptr;
+	str.size = 0;
+
+	return str;
+}
+
+laStrFixedUTF8 la_str_fixed_init_str(laStrUTF8* str)
+{
+	if (!str)
+		return la_str_fixed_init();
+
+	laStrFixedUTF8 strFixed;
+	strFixed.data = str->data;
+	strFixed.size = str->size;
+
+	return strFixed;
+}
+
+laStrFixedUTF8 la_str_fixed_init_cstr(const char* cstr)
+{
+	if (!cstr || (*cstr == '\0'))
+		return la_str_fixed_init();
+
+	laStrFixedUTF8 str;
+	str.data = cstr;
+	str.size = std::strlen(cstr);
+
+	return str;
+}
+
+int la_find_ctx_valid(const wclFindContext* ctx)
+{
+	return static_cast<int>(reinterpret_cast<const la::LinesRepo::FindContext*>(ctx)->isValid());
+}
+
+laStrUTF8 la_find_ctx_query(const wclFindContext* ctx)
+{
+	auto nCtx = reinterpret_cast<const la::LinesRepo::FindContext*>(ctx);
+	return convertStr(nCtx->query());
+}
+
+int la_find_ctx_line_position(const wclFindContext* ctx, int* lineIndex)
+{
+	auto nCtx = reinterpret_cast<const la::LinesRepo::FindContext*>(ctx);
+	auto pos = nCtx->position();
+
+	if (lineIndex)
+		*lineIndex = static_cast<int>(std::get<1>(pos));
+
+	return static_cast<int>(std::get<0>(pos));
+}
+
+laStrUTF8* la_list_files(laFlavorType flavor, laStrFixedUTF8 folderPath, int* numFiles)
+{
+	if (!folderPath.data || (folderPath.size <= 0))
+		return nullptr;
+
 	if (!numFiles)
 		return nullptr;
 
-	auto files = la::LinesRepo::listFolderFiles(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath, std::strlen(folderPath) });
+	*numFiles = 0;
 
-	//TODO
-	return nullptr;
-}
-
-wclLinesRepo* la_init_repo_file(laFlavorType flavor, const char* filePath)
-{
-	if (!filePath)
+	auto files = la::LinesRepo::listFolderFiles(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath.data, static_cast<size_t>(folderPath.size) });
+	if (files.empty())
 		return nullptr;
 
-	auto repo = la::LinesRepo::initRepoFile(static_cast<la::FlavorsRepo::Type>(flavor), { filePath, std::strlen(filePath) });
+	auto strList = reinterpret_cast<laStrUTF8*>(std::malloc(sizeof(laStrUTF8) * files.size()));
+	if (!strList)
+		return nullptr;
+
+	*numFiles = files.size();
+
+	int i = 0;
+	for (const auto& str : files)
+		strList[i++] = convertStr(str);
+
+	return strList;
+}
+
+wclLinesRepo* la_init_repo_file(laFlavorType flavor, laStrFixedUTF8 filePath)
+{
+	if (!filePath.data || (filePath.size <= 0))
+		return nullptr;
+
+	auto repo = la::LinesRepo::initRepoFile(static_cast<la::FlavorsRepo::Type>(flavor), { filePath.data, static_cast<size_t>(filePath.size) });
 	return (repo ? reinterpret_cast<wclLinesRepo*>(repo.release()) : nullptr);
 }
 
-wclLinesRepo* la_init_repo_folder(laFlavorType flavor, const char* folderPath)
+wclLinesRepo* la_init_repo_folder(laFlavorType flavor, laStrFixedUTF8 folderPath)
 {
-	if (!folderPath)
+	if (!folderPath.data || (folderPath.size <= 0))
 		return nullptr;
 
-	auto repo = la::LinesRepo::initRepoFolder(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath, std::strlen(folderPath) });
+	auto repo = la::LinesRepo::initRepoFolder(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath.data, static_cast<size_t>(folderPath.size) });
 	return (repo ? reinterpret_cast<wclLinesRepo*>(repo.release()) : nullptr);
 }
 
-wclLinesRepo* la_init_repo_folder_filter(laFlavorType flavor, const char* folderPath, const char* fileNameFilterRegex)
+wclLinesRepo* la_init_repo_folder_filter(laFlavorType flavor, laStrFixedUTF8 folderPath, laStrFixedUTF8 fileNameFilterRegex)
 {
-	if (!folderPath)
+	if (!folderPath.data || (folderPath.size <= 0))
 		return nullptr;
 
-	if (!fileNameFilterRegex || (*fileNameFilterRegex == '\0'))
+	if (!fileNameFilterRegex.data || (fileNameFilterRegex.size <= 0))
 		return la_init_repo_folder(flavor, folderPath);
 
-	auto repo = la::LinesRepo::initRepoFolder(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath, std::strlen(folderPath) }, { fileNameFilterRegex, std::strlen(fileNameFilterRegex) });
+	auto repo = la::LinesRepo::initRepoFolder(static_cast<la::FlavorsRepo::Type>(flavor), { folderPath.data, static_cast<size_t>(folderPath.size) }, { fileNameFilterRegex.data, static_cast<size_t>(fileNameFilterRegex.size) });
 	return (repo ? reinterpret_cast<wclLinesRepo*>(repo.release()) : nullptr);
+}
+
+wclLinesRepo* la_init_repo_command(wclLinesRepo* repo, laStrFixedUTF8 commandResult)
+{
+	if (!repo)
+		return nullptr;
+	if (!commandResult.data || (commandResult.size <= 0))
+		return nullptr;
+
+	auto newRepo = la::LinesRepo::initRepoFromCommnand(*reinterpret_cast<la::LinesRepo*>(repo), { commandResult.data, static_cast<size_t>(commandResult.size) });
+	return (newRepo ? reinterpret_cast<wclLinesRepo*>(newRepo.release()) : nullptr);
+}
+
+void la_repo_destroy(wclLinesRepo** repo)
+{
+	if (!repo)
+		return;
+
+	delete reinterpret_cast<la::LinesRepo*>(*repo);
+	*repo = nullptr;
 }
 
 int la_repo_num_files(wclLinesRepo* repo)
@@ -67,11 +257,102 @@ laFlavorType la_repo_flavor(wclLinesRepo* repo)
 	return static_cast<laFlavorType>(reinterpret_cast<la::LinesRepo*>(repo)->flavor());
 }
 
-void la_repo_destroy(wclLinesRepo** repo)
+wclFindContext* la_repo_search_text(wclLinesRepo* repo, laStrFixedUTF8 query, laSearchOptions searchOptions)
 {
 	if (!repo)
+		return nullptr;
+
+	auto fctx = reinterpret_cast<la::LinesRepo*>(repo)->searchText({ query.data, static_cast<size_t>(query.size) }, convertSearchOptions(searchOptions));
+	
+	auto wrapper = new la::LinesRepo::FindContext(std::move(fctx));
+	return (wrapper ? reinterpret_cast<wclFindContext*>(wrapper) : nullptr);
+}
+
+wclFindContext* la_repo_search_text_regex(wclLinesRepo* repo, laStrFixedUTF8 query, laSearchOptions searchOptions)
+{
+	if (!repo)
+		return nullptr;
+
+	auto fctx = reinterpret_cast<la::LinesRepo*>(repo)->searchTextRegex({ query.data, static_cast<size_t>(query.size) }, convertSearchOptions(searchOptions));
+
+	auto wrapper = new la::LinesRepo::FindContext(std::move(fctx));
+	return (wrapper ? reinterpret_cast<wclFindContext*>(wrapper) : nullptr);
+}
+
+void la_repo_search_next(wclLinesRepo* repo, wclFindContext* ctx)
+{
+	if (!repo || !ctx)
 		return;
 
-	delete reinterpret_cast<la::LinesRepo*>(repo);
-	*repo = nullptr;
+	auto nCtx = reinterpret_cast<la::LinesRepo::FindContext*>(ctx);
+	*nCtx = reinterpret_cast<la::LinesRepo*>(repo)->searchNext(*nCtx);
+}
+
+void la_repo_search_destroy(wclFindContext** ctx)
+{
+	if (!ctx)
+		return;
+
+	delete reinterpret_cast<la::LinesRepo::FindContext*>(*ctx);
+	*ctx = nullptr;
+}
+
+laStrUTF8 la_repo_retrieve_line_content(wclLinesRepo* repo, int lineIndex, laTranslatorType translatorType)
+{
+	if (!repo || (lineIndex < 0))
+		return la_str_init();
+
+	auto res = reinterpret_cast<la::LinesRepo*>(repo)->retrieveLineContent(static_cast<size_t>(lineIndex), static_cast<la::TranslatorsRepo::Type>(translatorType));
+	return convertStr(res);
+}
+
+laStrUTF8 la_repo_list_available_commands(wclLinesRepo* repo)
+{
+	if (!repo)
+		return la_str_init();
+
+	auto res = reinterpret_cast<la::LinesRepo*>(repo)->listAvailableCommands();
+	return convertStr(res);
+}
+
+laStrUTF8 la_repo_execute_command(wclLinesRepo* repo, laStrFixedUTF8 tag, laStrFixedUTF8 name)
+{
+	if (!repo)
+		return la_str_init();
+
+	auto res = reinterpret_cast<la::LinesRepo*>(repo)->executeCommand({ tag.data, static_cast<size_t>(tag.size) }, { name.data, static_cast<size_t>(name.size) });
+	return convertStr(res);
+}
+
+laStrUTF8 la_repo_execute_command_params(wclLinesRepo* repo, laStrFixedUTF8 tag, laStrFixedUTF8 name, laStrFixedUTF8 params)
+{
+	if (!repo)
+		return la_str_init();
+
+	auto res = reinterpret_cast<la::LinesRepo*>(repo)->executeCommand({ tag.data, static_cast<size_t>(tag.size) }, { name.data, static_cast<size_t>(name.size) }, { params.data, static_cast<size_t>(params.size) });
+	return convertStr(res);
+}
+
+int la_repo_export_lines(wclLinesRepo* repo, const laExportOptions* options, int indexStart, int count)
+{
+	if (!repo || (indexStart < 0) || (count < 0))
+		return 0;
+
+	return reinterpret_cast<la::LinesRepo*>(repo)->exportLines(convertExportOptions(options), static_cast<size_t>(indexStart), static_cast<size_t>(count));
+}
+
+int la_repo_export_command_lines(wclLinesRepo* repo, const laExportOptions* options, laStrFixedUTF8 commandResult)
+{
+	if (!repo || (commandResult.size <= 0))
+		return 0;
+
+	return reinterpret_cast<la::LinesRepo*>(repo)->exportCommandLines(convertExportOptions(options), { commandResult.data, static_cast<size_t>(commandResult.size) });
+}
+
+int la_repo_export_command_network_packets(wclLinesRepo* repo, const laExportOptions* options, laStrFixedUTF8 commandResult)
+{
+	if (!repo || (commandResult.size <= 0))
+		return 0;
+
+	return reinterpret_cast<la::LinesRepo*>(repo)->exportCommandNetworkPackets(convertExportOptions(options), { commandResult.data, static_cast<size_t>(commandResult.size) });
 }
--- shared/api.h
+++ shared/api.h
@@ -19,33 +19,106 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 typedef enum {
 	LA_FLAVOR_TYPE_UNKNOWN,
 	LA_FLAVOR_TYPE_WCSCOMLIB,
 	LA_FLAVOR_TYPE_WCSSERVER,
-	LA_FLAVOR_TYPE_WCSANDROIDLOGCAT,
+	LA_FLAVOR_TYPE_WCSANDROIDLOGCAT
 } laFlavorType;
+
+typedef enum {
+	LA_TRANSLATOR_TYPE_RAW,
+	LA_TRANSLATOR_TYPE_RAW_JSON,
+	LA_TRANSLATOR_TYPE_TRANSLATED,
+	LA_TRANSLATOR_TYPE_TRANSLATED_JSON
+} laTranslatorType;
+
+typedef enum {
+	LA_SEARCH_OPTION_NONE,
+	LA_SEARCH_OPTION_CASE_SENSITIVE
+} laSearchOptions;
+
+typedef struct
+{
+	char* data;
+	int size;
+} laStrUTF8;
+
+typedef struct
+{
+	const char* data;
+	int size;
+} laStrFixedUTF8;
+
+typedef struct
+{
+	int8_t appendToFile;
+	laStrFixedUTF8 filePath;
+	laTranslatorType translationType;
+} laExportOptions;
+
+typedef struct wclFindContext wclFindContext;
 
 typedef struct wclLinesRepo wclLinesRepo;
 
-LA_API_VISIBILITY const char** la_list_files(laFlavorType flavor, const char* folderPath, int* numFiles);
+/********
+** Utils
+********/
 
-LA_API_VISIBILITY wclLinesRepo* la_init_repo_file(laFlavorType flavor, const char* filePath);
-LA_API_VISIBILITY wclLinesRepo* la_init_repo_folder(laFlavorType flavor, const char* folderPath);
-LA_API_VISIBILITY wclLinesRepo* la_init_repo_folder_filter(laFlavorType flavor, const char* folderPath, const char* fileNameFilterRegex);
+LA_API_VISIBILITY laStrUTF8 la_str_init();
+LA_API_VISIBILITY laStrUTF8 la_str_init_copy(const char* cstr);
+LA_API_VISIBILITY laStrUTF8 la_str_init_length(int size);
+LA_API_VISIBILITY void la_str_destroy(laStrUTF8* str);
+
+LA_API_VISIBILITY laStrFixedUTF8 la_str_fixed_init();
+LA_API_VISIBILITY laStrFixedUTF8 la_str_fixed_init_str(laStrUTF8* str);
+LA_API_VISIBILITY laStrFixedUTF8 la_str_fixed_init_cstr(const char* cstr);
+
+LA_API_VISIBILITY int la_find_ctx_valid(const wclFindContext* ctx);
+LA_API_VISIBILITY laStrUTF8 la_find_ctx_query(const wclFindContext* ctx);
+LA_API_VISIBILITY int la_find_ctx_line_position(const wclFindContext* ctx, int* lineIndex);
+
+
+/********
+** Repo init and file search
+********/
+
+LA_API_VISIBILITY laStrUTF8* la_list_files(laFlavorType flavor, laStrFixedUTF8 folderPath, int* numFiles);
+
+LA_API_VISIBILITY wclLinesRepo* la_init_repo_file(laFlavorType flavor, laStrFixedUTF8 filePath);
+LA_API_VISIBILITY wclLinesRepo* la_init_repo_folder(laFlavorType flavor, laStrFixedUTF8 folderPath);
+LA_API_VISIBILITY wclLinesRepo* la_init_repo_folder_filter(laFlavorType flavor, laStrFixedUTF8 folderPath, laStrFixedUTF8 fileNameFilterRegex);
+
+LA_API_VISIBILITY wclLinesRepo* la_init_repo_command(wclLinesRepo* repo, laStrFixedUTF8 commandResult);
+
+LA_API_VISIBILITY void la_repo_destroy(wclLinesRepo** repo);
+
+/********
+** Repo operations
+********/
 
 LA_API_VISIBILITY int la_repo_num_files(wclLinesRepo* repo);
 LA_API_VISIBILITY int la_repo_num_lines(wclLinesRepo* repo);
 LA_API_VISIBILITY laFlavorType la_repo_flavor(wclLinesRepo* repo);
 
-/*LA_API_VISIBILITY const char* la_lines_repo_line_content(wclLinesRepo* repo, int lineIndex, int process);
 
-LA_API_VISIBILITY const char* la_lines_repo_available_commands(wclLinesRepo* repo);
+LA_API_VISIBILITY wclFindContext* la_repo_search_text(wclLinesRepo* repo, laStrFixedUTF8 query, laSearchOptions searchOptions);
+LA_API_VISIBILITY wclFindContext* la_repo_search_text_regex(wclLinesRepo* repo, laStrFixedUTF8 query, laSearchOptions searchOptions);
+LA_API_VISIBILITY void la_repo_search_next(wclLinesRepo* repo, wclFindContext* ctx);
+LA_API_VISIBILITY void la_repo_search_destroy(wclFindContext** ctx);
 
-LA_API_VISIBILITY const char* la_lines_repo_execute_command(wclLinesRepo* repo, const char* appName, const char* commandName);
-LA_API_VISIBILITY const char* la_lines_repo_execute_command_params(wclLinesRepo* repo, const char* appName, const char* commandName, const char* params);*/
+LA_API_VISIBILITY laStrUTF8 la_repo_retrieve_line_content(wclLinesRepo* repo, int lineIndex, laTranslatorType translatorType);
 
-LA_API_VISIBILITY void la_repo_destroy(wclLinesRepo** repo);
+LA_API_VISIBILITY laStrUTF8 la_repo_list_available_commands(wclLinesRepo* repo);
+
+LA_API_VISIBILITY laStrUTF8 la_repo_execute_command(wclLinesRepo* repo, laStrFixedUTF8 tag, laStrFixedUTF8 name);
+LA_API_VISIBILITY laStrUTF8 la_repo_execute_command_params(wclLinesRepo* repo, laStrFixedUTF8 tag, laStrFixedUTF8 name, laStrFixedUTF8 params);
+
+LA_API_VISIBILITY int la_repo_export_lines(wclLinesRepo* repo, const laExportOptions* options, int indexStart, int count);
+LA_API_VISIBILITY int la_repo_export_command_lines(wclLinesRepo* repo, const laExportOptions* options, laStrFixedUTF8 commandResult);
+LA_API_VISIBILITY int la_repo_export_command_network_packets(wclLinesRepo* repo, const laExportOptions* options, laStrFixedUTF8 commandResult);
 
 #ifdef  __cplusplus
 }
